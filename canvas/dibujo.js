04e70bec6c6c33e184aa6e65fb8ab621fce936a6
Jhon <jon.cl371@gmail.com>
2019-01-21 17:04:46 -0400

    cuadricula 4 lados prueba

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -3,22 +3,27 @@ var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 var lineas = 30;
 var l = 0;
-var xf, yi;
+var xf, yi, nxf, nyi;
 var colorcito = '#AFA';
 
-while (l < lineas)
+
+for (l = 0; l < lineas; l++)
 {
   yi = 10 * l;
   xf = 10 * (l + 1);
+  nxf = 300 - (l * 10);
+  nyi = 300 - (l * 10);
   dibujarLinea('#90CAF9', 0, yi, xf, 300);
-  console.log("linea " + l);
-  l++; //* l = l + 1;
+  dibujarLinea('#B2EBF2', 300, yi, nxf, 300);
+  dibujarLinea('#B2EBF2', 0, nyi, xf, 0);
+  dibujarLinea('#FF4081', 300, yi, xf, 0);
 
+  console.log("linea " + l);
+  l = l + 1; // l++;
 }
 
 dibujarLinea(colorcito, 1, 1, 1, 300);
 dibujarLinea(colorcito, 1, 299, 299, 299);
-
 
 function dibujarLinea(color, x_inicial, y_inicial, x_final, y_final)
 {
