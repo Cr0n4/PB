b75d993932ffd924a3426f9af3964aa3fcdcfa2f
Jhon <jon.cl371@gmail.com>
2019-01-21 15:52:26 -0400

    cuadricula con while

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,8 +1,24 @@
 
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var lineas = 30;
+var l = 0;
+var xf, yi;
+var colorcito = '#AFA';
 
-dibujarLinea('#90CAF9', 0, 0, 10, 300);
+while (l < lineas)
+{
+  yi = 10 * l;
+  xf = 10 * (l + 1);
+  dibujarLinea('#90CAF9', 0, yi, xf, 300);
+  console.log("linea " + l);
+  l++; //* l = l + 1;
+
+}
+
+dibujarLinea(colorcito, 1, 1, 1, 300);
+dibujarLinea(colorcito, 1, 299, 299, 299);
+
 
 function dibujarLinea(color, x_inicial, y_inicial, x_final, y_final)
 {
