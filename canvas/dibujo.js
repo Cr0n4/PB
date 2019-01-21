ca12207cef4cee137069aa5fcce90d98ff907da1
Jhon <jon.cl371@gmail.com>
2019-01-21 15:33:18 -0400

    creando una cuadricula con funciones

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -2,9 +2,14 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 
-lienzo.beginPath();
-lienzo.strokeStyle = "red";
-lienzo.moveTo(100,100);
-lienzo.lineTo(200,200);
-lienzo.stroke();
-lienzo.closePath();
+dibujarLinea('#90CAF9', 0, 0, 10, 300);
+
+function dibujarLinea(color, x_inicial, y_inicial, x_final, y_final)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color;
+  lienzo.moveTo(x_inicial,y_inicial);
+  lienzo.lineTo(x_final,y_final);
+  lienzo.stroke();
+  lienzo.closePath();
+}
