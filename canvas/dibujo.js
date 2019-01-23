d2e7b3418caa10de2e9e78d62ca8ab1d4020f533
Jhon <jon.cl371@gmail.com>
2019-01-23 16:50:59 -0400

    modificando dibujo

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,23 +1,10 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick);
 
 var d = document.getElementById("dibujito");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-var lineas = 30;
-var l = 0;
-var xf, yi, nxf, nyi;
-var colorcito = '#AFA';
-
-
-for (l = 0; l < lineas; l++)
-{
-  yi = 10 * l;
-  xf = 10 * (l + 1);
-  dibujarLinea('#90CAF9', 0, yi, xf, 300);
-  console.log("linea " + l);
-  l = l + 1; // l++;
-}
-
-dibujarLinea(colorcito, 1, 1, 1, 300);
-dibujarLinea(colorcito, 1, 299, 299, 299);
 
 function dibujarLinea(color, x_inicial, y_inicial, x_final, y_final)
 {
@@ -27,4 +14,24 @@ function dibujarLinea(color, x_inicial, y_inicial, x_final, y_final)
   lienzo.lineTo(x_final,y_final);
   lienzo.stroke();
   lienzo.closePath();
+}
+
+function dibujoPorClick()
+{
+  var lineas = parseInt(texto.value);
+  var l = 0;
+  var xf, yi;
+  var colorcito = '#AFA';
+  var espacio = ancho / lineas;
+
+  for (l = 0; l < lineas; l++)
+  {
+    yi = espacio * l;
+    xf = espacio * (l + 1);
+    dibujarLinea('#90CAF9', 0, yi, xf, 300);
+    console.log("linea " + l);
+  }
+
+  dibujarLinea(colorcito, 1, 1, 1, 300);
+  dibujarLinea(colorcito, 1, 299, 299, 299);
 }
