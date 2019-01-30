5ef976237559f0382ff61c13563508c4240b915e
Jhon <jon.cl371@gmail.com>
2019-01-30 18:23:05 -0400

    reconocer teclas

--- teclas/eventos.js
+++ teclas/eventos.js
@@ -1,1 +1,32 @@
-alert("todo bien");
+var teclas = {
+  UP: 38,
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT: 39
+};
+
+console.log(teclas);
+
+document.addEventListener("keyup", dibujarTeclado);
+
+function dibujarTeclado(evento)
+{
+  switch (evento.keyCode)
+  {
+    case teclas.UP:
+      console.log("Arriba");
+      break;
+    case teclas.DOWN:
+      console.log("Abajo");
+      break;
+    case teclas.LEFT:
+      console.log("Izquierda");
+      break;
+    case teclas.RIGHT:
+      console.log("derecha")
+      break;
+    default:
+      console.log("otra tecla");
+
+  }
+}
