8c90d07d9727c4e9032b1979ceebe903ebd4d719
Jhon <jon.cl371@gmail.com>
2019-02-05 18:52:11 -0400

    villa con cerdos pollos y vacas

--- villa/platzi.js
+++ villa/platzi.js
@@ -13,10 +13,15 @@ var cerdo = {
   url:"cerdo.png",
   cargaOk: false
 }
+var pollo = {
+  url:"pollo.png",
+  cargaOk: false
+}
 
 
 var cantidad = aleatorio(0, 10);
 var cantidadC = aleatorio(0, 5);
+var cantidadP = aleatorio(0, 5);
 
 fondo.imagen = new Image();
 fondo.imagen.src = fondo.url;
@@ -29,6 +34,10 @@ vaca.imagen.addEventListener("load", cargarVacas);
 cerdo.imagen = new Image();
 cerdo.imagen.src = cerdo.url;
 cerdo.imagen.addEventListener("load", cargarCerdos);
+
+pollo.imagen = new Image();
+pollo.imagen.src = pollo.url;
+pollo.imagen.addEventListener("load", cargarPollos);
 
 function cargarFondo()
 {
@@ -43,6 +52,11 @@ function cargarVacas()
 function cargarCerdos()
 {
   cerdo.cargaOk = true;
+  dibujar();
+}
+function cargarPollos()
+{
+  pollo.cargaOk = true;
   dibujar();
 }
 
@@ -71,11 +85,23 @@ function dibujar()
     console.log(cantidadC);
     for (var c=0; c < cantidadC; c++)
     {
-      var x = aleatorio(0,5);
-      var y = aleatorio(0,5);
-      var x = x * 60;
-      var y = y * 60;
+      var x = aleatorio(0,4);
+      var y = aleatorio(0,4);
+      var x = x * 70;
+      var y = y * 70;
       papel.drawImage(cerdo.imagen, x, y);
+    }
+  }
+  if (pollo.cargaOk)
+  {
+    console.log(cantidadP);
+    for (var p=0; p < cantidadP; p++)
+    {
+      var x = aleatorio(0,7);
+      var y = aleatorio(0,7);
+      var x = x * 40;
+      var y = y * 40;
+      papel.drawImage(pollo.imagen, x, y);
     }
   }
 }
